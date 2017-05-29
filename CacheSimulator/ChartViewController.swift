b3f6ee9a9b682d42a7deaca46a69281da5b3c52a
abradat <shayankhorsandi1376@gmail.com>
2017-05-29 11:51:00 +0430

    an

--- CacheSimulator/ChartViewController.swift
+++ CacheSimulator/ChartViewController.swift
@@ -11,6 +11,14 @@ import ScrollableGraphView
 
 class ChartViewController : UIViewController{
     
+    var graphView :ScrollableGraphView? = nil
+    
+    @IBAction func a(_ sender: Any) {
+        let adata: [Double] = [4, 8, 15, 16, 23, 42, 48, 50, 20]
+        let alabels = ["one", "two", "three", "four", "five", "six", "seven", "eight", "an"]
+        
+        self.graphView?.set(data: adata, withLabels: alabels)
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -27,33 +35,35 @@ class ChartViewController : UIViewController{
     //@IBOutlet weak var barChart: BarChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let graphView = ScrollableGraphView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * (2/3)))
+        //let graphView = ScrollableGraphView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * (2/3)))
+        self.graphView = ScrollableGraphView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * (2/3)))
+        self.graphView?.shouldAnimateOnStartup = true
         let data: [Double] = [4, 8, 15, 16, 23, 42, 48, 50]
         
         let labels = ["one", "two", "three", "four", "five", "six", "seven", "eight"]
         //graphView.setData(data, withLabels: labels)
-        graphView.set(data: data, withLabels: labels)
-        graphView.shouldAdaptRange = true
-        graphView.shouldAnimateOnAdapt = true
+        self.graphView?.set(data: data, withLabels: labels)
+        self.graphView?.shouldAdaptRange = true
+        self.graphView?.shouldAnimateOnAdapt = true
         //graphView.backgroundFillColor = UIColor.colorFromHex("#00BFFF")
-        graphView.backgroundFillColor = UIColor(red:0.00, green:0.75, blue:1.00, alpha:1.0)
+        self.graphView?.backgroundFillColor = UIColor(red:0.00, green:0.75, blue:1.00, alpha:1.0)
         //graphView.lineColor = UIColor.clearColor
         
-        graphView.dataPointSize = 5
-        graphView.dataPointSpacing = 80
-        graphView.dataPointLabelFont = UIFont.boldSystemFont(ofSize: 10)
-        graphView.dataPointLabelColor = UIColor.white
-        graphView.dataPointFillColor = UIColor.white
+        self.graphView?.dataPointSize = 5
+        self.graphView?.dataPointSpacing = 80
+        self.graphView?.dataPointLabelFont = UIFont.boldSystemFont(ofSize: 10)
+        self.graphView?.dataPointLabelColor = UIColor.white
+        self.graphView?.dataPointFillColor = UIColor.white
         
-        graphView.referenceLineLabelFont = UIFont.boldSystemFont(ofSize: 10)
-        graphView.referenceLineColor = UIColor.white.withAlphaComponent(0.5)
-        graphView.referenceLineLabelColor = UIColor.white
+        self.graphView?.referenceLineLabelFont = UIFont.boldSystemFont(ofSize: 10)
+        self.graphView?.referenceLineColor = UIColor.white.withAlphaComponent(0.5)
+        self.graphView?.referenceLineLabelColor = UIColor.white
         
-        graphView.numberOfIntermediateReferenceLines = 9
+        self.graphView?.numberOfIntermediateReferenceLines = 9
         
-        graphView.rangeMax = 50
+        self.graphView?.rangeMax = 50
         
-        self.view.addSubview(graphView)
+        self.view.addSubview((self.graphView)!)
         
     }
 }
--- CacheSimulator/SimulatorViewController.swift
+++ CacheSimulator/SimulatorViewController.swift
@@ -7,29 +7,70 @@
 //
 
 import UIKit
+import ScrollableGraphView
 
 class SimulatorViewController : UIViewController {
     
-    var addresses = Addresses()
+    var graphView :ScrollableGraphView? = nil
+    var navigationBarAppearace = UINavigationBar.appearance()
+    
+    
+    var range : [String] = []
+    var hitRate : [Int] = []
+    var addresses : Addresses = Addresses()
     var addressType : String!
+    var flag : [Int] = [0, 0, 0]
+    var defaultBarColor : UIColor?
+    var defaultColor : UIColor?
+    
     var directMapping : DirectMapping! {
         didSet{
             navigationItem.title = "Direct Mapping"
+            self.flag[0] = 1
+            
         }
     }
     var fullAssociative : FullyAssociative! {
         didSet {
             navigationItem.title = "Fully Associative"
+            self.flag[1] = 1
         }
     }
     
     var setAssociative : SetAssociative! {
         didSet {
             navigationItem.title = "Set Associative"
+            self.flag[2] = 1
         }
+    }
+    override func loadView() {
+        super.loadView()
+        //navigationController?.navigationBar.backgroundColor = UIColor.black
+        self.defaultBarColor = tabBarController?.tabBar.barTintColor
+        self.defaultColor = tabBarController?.tabBar.tintColor
+
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.tabBarController?.tabBar.isHidden = true
+        self.navigationController?.navigationBar.isHidden = true
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+
+        self.navigationController?.navigationBar.isHidden = false
+        self.tabBarController?.tabBar.isHidden = false
+        
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(addressType)
+        
+        
     }
+    
+    
 }
